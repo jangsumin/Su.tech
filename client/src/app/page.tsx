311a65c7030a7510d6905c189e5e8379c483281f
jangsumin <jsm0726@naver.com>
2024-08-06 00:20:02 +0900

    refactor: Carousel 컴포넌트를 합성 컴포넌트 패턴으로 리팩토링

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -8,14 +8,27 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <Carousel />
+
+      <Carousel>
+        <Carousel.Content></Carousel.Content>
+
+        <Carousel.PointerGroup>
+          <Carousel.PointerGroup.Pointer isSelected={false} />
+          <Carousel.PointerGroup.Pointer isSelected={true} />
+          <Carousel.PointerGroup.Pointer isSelected={false} />
+          <Carousel.PointerGroup.Pointer isSelected={false} />
+        </Carousel.PointerGroup>
+      </Carousel>
+
       <Tab />
+
       <PostPreview
         title="합성 컴포넌트 패턴, 정체를 밝혀라."
         subscription="복잡한 컴포넌트의 구성 요소를 조합해서 만드는 방법을 소개합니다."
         createdAt="2024월 8월 9일"
         thumbnailSrc="/images/pipe.png"
       />
+
       <PopularPost />
     </>
   );
--- client/src/components/carousel/Carousel.tsx
+++ client/src/components/carousel/Carousel.tsx
@@ -1,39 +1,10 @@
-"use client";
+import CarouselWrapper from "./CarouselWrapper";
+import Content from "./Content";
+import PointerGroup from "./PointerGroup";
 
-import "./Carousel.css";
-import { useState } from "react";
-
-function Carousel() {
-  const [step, setStep] = useState<number>(0);
-
-  const handleSlider = (step: number) => {
-    setStep(step);
-  };
-
-  return (
-    <div className="carousel-wrapper">
-      {step === 0 && <div className="content"></div>}
-
-      <ul className="slider-wrapper">
-        <li
-          className={`slider ${step === 0 ? "selected" : "non-selected"}`}
-          onClick={() => handleSlider(0)}
-        ></li>
-        <li
-          className={`slider ${step === 1 ? "selected" : "non-selected"}`}
-          onClick={() => handleSlider(1)}
-        ></li>
-        <li
-          className={`slider ${step === 2 ? "selected" : "non-selected"}`}
-          onClick={() => handleSlider(2)}
-        ></li>
-        <li
-          className={`slider ${step === 3 ? "selected" : "non-selected"}`}
-          onClick={() => handleSlider(3)}
-        ></li>
-      </ul>
-    </div>
-  );
-}
+const Carousel = Object.assign(CarouselWrapper, {
+  Content: Content,
+  PointerGroup: PointerGroup,
+});
 
 export default Carousel;
