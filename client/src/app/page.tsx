92e3a83caa8f9b8320b080b2df4ab33752f133be
jangsumin <jsm0726@naver.com>
2024-08-07 17:08:36 +0900

    feat: Tab 컴포넌트 생성 - close #3

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Navbar from "@/components/navbar/Navbar";
+import Tab from "@/components/tab/Tab";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+
+      <Tab />
     </>
   );
 }
