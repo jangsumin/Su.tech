9eddd0537a4f305764f32a1add145e28c571b976
jangsumin <jsm0726@naver.com>
2024-08-13 15:00:45 +0900

    style: module CSS로 Navbar 컴포넌트를 스타일링

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import "./navbar.css";
+import styles from "./Navbar.module.css";
+import classnames from "classnames/bind";
+
+const cx = classnames.bind(styles);
 
 function Navbar() {
   return (
-    <nav className="navbar-wrapper">
+    <nav className={cx("navbar-wrapper")}>
       <Image
-        className="logo-icon"
+        className={cx("logo-icon")}
         src="/su-tech.svg"
         width={130}
         height={43}
         alt="logo"
       />
-      <MagnifyingGlassIcon className="search-icon" title="search" />
+      <MagnifyingGlassIcon className={cx("search-icon")} title="search" />
     </nav>
   );
 }
