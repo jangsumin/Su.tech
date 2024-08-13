8bc8a56d0ea5de38402b83f12e4ff5b17f186931
jangsumin <jsm0726@naver.com>
2024-08-13 14:58:47 +0900

    style: module CSS로 PopularPost 컴포넌트를 스타일링

--- client/src/components/popular-post/PopularPost.tsx
+++ client/src/components/popular-post/PopularPost.tsx
@@ -1,21 +1,24 @@
-import "./PopularPost.css";
+import styles from "./PopularPost.module.css";
+import classnames from "classnames/bind";
+
+const cx = classnames.bind(styles);
 
 const PopularPost = () => {
   return (
-    <section className="popular-post-wrapper">
-      <span className="heading-text">인기있는 글</span>
+    <section className={cx("popular-post-wrapper")}>
+      <span className={cx("heading-text")}>인기있는 글</span>
 
-      <ul className="popular-post-list">
-        <li className="popular-post-element">
-          <h1 className="title">합성 컴포넌트 패턴, 정체를 밝혀라.</h1>
-          <span className="tag">React·UI·Component</span>
+      <ul className={cx("popular-post-list")}>
+        <li className={cx("popular-post-element")}>
+          <h1 className={cx("title")}>합성 컴포넌트 패턴, 정체를 밝혀라.</h1>
+          <span className={cx("tag")}>React·UI·Component</span>
         </li>
 
-        <li className="popular-post-element">
-          <h1 className="title">
+        <li className={cx("popular-post-element")}>
+          <h1 className={cx("title")}>
             얼마 전에 있었던 XXXX 면접 후기, 그리고 뼈 아픔
           </h1>
-          <span className="tag">XXXX·면접후기</span>
+          <span className={cx("tag")}>XXXX·면접후기</span>
         </li>
       </ul>
     </section>
