6b2a9d126765f7ac4813a9dcc2959645028ad39f
jangsumin <jsm0726@naver.com>
2024-08-16 13:53:00 +0900

    style: 홈페이지를 이루는 컴포넌트 간 레이아웃 조정 - close #20

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,6 +5,10 @@ import Tab from "@/components/tab/Tab";
 import PostPreview from "@/components/post-preview/PostPreview";
 import PopularPost from "@/components/popular-post/PopularPost";
 import Tag from "@/components/tag/Tag";
+import classnames from "classnames/bind";
+import styles from "./page.module.css";
+
+const cx = classnames.bind(styles);
 
 export default function Home() {
   return (
@@ -27,26 +31,39 @@ export default function Home() {
         <Indicators />
       </Carousel>
 
-      <Tab />
+      <div className={cx("content-wrapper")}>
+        <div className={cx("main-content-wrapper")}>
+          <Tab />
 
-      <PostPreview
-        title="합성 컴포넌트 패턴, 정체를 밝혀라."
-        subscription="복잡한 컴포넌트의 구성 요소를 조합해서 만드는 방법을 소개합니다."
-        createdAt="2024월 8월 9일"
-        thumbnailSrc="/images/pipe.png"
-      />
+          <PostPreview
+            title="합성 컴포넌트 패턴, 정체를 밝혀라."
+            subscription="복잡한 컴포넌트의 구성 요소를 조합해서 만드는 방법을 소개합니다."
+            createdAt="2024월 8월 16일"
+            thumbnailSrc="/images/pipe.png"
+          />
 
-      <PopularPost />
+          <PostPreview
+            title="합성 컴포넌트 패턴, 정체를 밝혀라."
+            subscription="복잡한 컴포넌트의 구성 요소를 조합해서 만드는 방법을 소개합니다."
+            createdAt="2024월 8월 9일"
+            thumbnailSrc="/images/pipe.png"
+          />
+        </div>
 
-      <Tag
-        tagList={[
-          { tagId: 1, tagName: "Frontend" },
-          { tagId: 2, tagName: "Backend" },
-          { tagId: 3, tagName: "생산성" },
-          { tagId: 4, tagName: "React" },
-          { tagId: 5, tagName: "storybook" },
-        ]}
-      />
+        <div className={cx("sub-content-wrapper")}>
+          <PopularPost />
+
+          <Tag
+            tagList={[
+              { tagId: 1, tagName: "Frontend" },
+              { tagId: 2, tagName: "Backend" },
+              { tagId: 3, tagName: "생산성" },
+              { tagId: 4, tagName: "React" },
+              { tagId: 5, tagName: "storybook" },
+            ]}
+          />
+        </div>
+      </div>
     </>
   );
 }
